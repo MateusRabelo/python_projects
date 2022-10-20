e2d177009cca15a6b26954d880acc5483c1eeac2
MateusRabelo <mateusaqles@gmail.com>
2022-10-19 23:48:54 -0300

    A madlibs example

---
+++ madlibs/madlibs.py
@@ -0,0 +1,6 @@
+bad_adj = input("A bad adjetive: ")
+your_name = input("Your name: ")
+
+madlib = f"Computer science can be very {bad_adj} at times, but it sure opens up a lot of opportunities and is so worth it.\nAnd i {your_name} will be a winner! :)"#.format(bad_adj, your_name)
+
+print(madlib)
